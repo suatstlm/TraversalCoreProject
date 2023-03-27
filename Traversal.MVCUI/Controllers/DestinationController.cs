a0fafdaed4ba219032817cb096674da372ca96ec
SUAT SATILMIŞ <suat.satilmis@ym.com.tr>
2023-03-27 21:25:47 +0300

    UI added some feature

---
+++ Traversal.MVCUI/Controllers/DestinationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Traversal.BusinessLayer.Concrete;
+using Traversal.DataAccesLayer.EntityFramework;
+using Traversal.EntityLayer.Concrete;
+
+namespace Traversal.MVCUI.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager _destinationManager = new(new EfDestinationDal());
+
+        public IActionResult Index()
+        {
+            var result = _destinationManager.TGetAll();
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination destination)
+        {
+            return View();
+        }
+    }
+}
--- Traversal.MVCUI/ViewComponents/Default/_Testimonial.cs
+++ Traversal.MVCUI/ViewComponents/Default/_Testimonial.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Traversal.BusinessLayer.Concrete;
+using Traversal.DataAccesLayer.EntityFramework;
 
 namespace Traversal.MVCUI.ViewComponents.Default
 {
     public class _Testimonial: ViewComponent
     {
+        TestimonialManager _testimonialManager = new(new EfTestimonialDal());
+
         public IViewComponentResult Invoke()
         {
-            return View();  
+            var result = _testimonialManager.TGetAll();
+            return View(result);  
         }
     }
 }
